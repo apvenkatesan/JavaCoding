88d01b30834aaa76956e85ed016c987d37361530
swarnalatha <swarnalatha@Venkat-Desktop>
2026-07-06 22:34:54 +0530

    SeparationOfEvenOddNumberInList.java

---
+++ InterviewPrograms/src/com/venkat/SeparationOfEvenOddNumberInList.java
@@ -0,0 +1,23 @@
+package com.venkat;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import static java.util.stream.Collectors.*;
+
+public class SeparationOfEvenOddNumberInList {
+
+	public static void main(String[] args) {
+		List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+		
+		Map<Boolean, List<Integer>> oddAndEvenList = numbers.stream()
+				.collect(Collectors.partitioningBy(n -> n %2 == 0));
+				
+		System.out.println("Even numbers in the list are: " + oddAndEvenList.get(true));
+		System.out.println("Odd numbers in the list are: " + oddAndEvenList.get(false));
+		
+
+	}
+
+}
