a669170d0927cf94058d3c72f57a780f31e1ca5f
swarnalatha <swarnalatha@Venkat-Desktop.mshome.net>
2026-07-07 09:25:49 +0530

    Renamed the fileName

---
+++ InterviewPrograms/src/com/venkat/SeparationOfEvenOddNumberInMap.java
@@ -0,0 +1,39 @@
+package com.venkat;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+
+
+public class SeparationOfEvenOddNumberInMap {
+
+	public static void main(String[] args) {
+		List<Integer> numbers = List.of(1,2,3,4,5,6,7,8,9,10);
+
+		Map<Boolean, List<Integer>> oddOrEvenNumbers = numbers.stream()
+				.collect(Collectors.partitioningBy(n->n%2 ==0));
+		
+		System.out.println("Even Numbers List is " + oddOrEvenNumbers.get(true));
+		System.out.println("Odd Numbers List is " + oddOrEvenNumbers.get(false));
+		
+		List<Integer> oddNumbers = new ArrayList<>();
+		List<Integer> evenNumbers = new ArrayList<>();
+		
+		numbers.stream()
+		.forEach(n->{
+			if (n%2 == 0) {
+				evenNumbers.add(n);
+			} else {
+				oddNumbers.add(n);
+			}
+		});
+
+		System.out.println("Even Numbers List is " + evenNumbers);
+		System.out.println("Odd Numbers List is " + oddNumbers);
+
+
+	}
+}
